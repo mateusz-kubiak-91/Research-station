3b8f66d0b8a1999e17abe4da94366778ff00ffe7
Mateusz Kubiak <mateusz.kubiak.1991@gmail.com>
2021-01-31 19:35:21 +0100

    Add base files

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const yesBtn = document.querySelector('.yesBtn');
+const noBtn = document.querySelector('.noBtn');
+let financeBar = document.querySelector('.financeBar'); // financeBar.style.width = "20%"
+let moraleBar = document.querySelector('.moraleBar');
+let researchBar = document.querySelector('.researchBar');
+
