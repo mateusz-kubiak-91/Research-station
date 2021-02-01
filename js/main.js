1b32e0cc36f226a4c680110e52f8437783ecfc76
Mateusz Kubiak <mateusz.kubiak.1991@gmail.com>
2021-02-01 22:42:47 +0100

    add welcom page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 const yesBtn = document.querySelector('.yesBtn');
 const noBtn = document.querySelector('.noBtn');
+const startBtn = document.querySelector('.startBtn');
+
+let welcomDisplay = document.querySelector('div.welcome')
+let containerDisplay = document.querySelector('div.container')
 let financeBar = document.querySelector('.financeBar'); // financeBar.style.width = "20%"
 let moraleBar = document.querySelector('.moraleBar');
 let researchBar = document.querySelector('.researchBar');
 
+startBtn.addEventListener('click', () => {
+    welcomDisplay.style.display = 'none'
+    containerDisplay.style.display = 'grid'
+})
